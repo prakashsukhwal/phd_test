b26803b28646f29dd9a6fb34e6ab80640bee22d5
Prakaash <prakash.sukhwal@gmail.com>
2024-01-26 16:45:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def app():
         st.subheader("Registration Instructions:")
         st.write("""
         Please complete the registration process to access the conversational agent. Follow these steps:
-        - Go to the menu on the left and select 'Registration'
+        - Go to the menu on the left and select 'Registration'...
         - Enter a username of choice in the provided field along with other details needed. 
         - Select the applicable 'Existing Health Conditions'.
         - Upload an icon or image as your profile picture. (This can be any random icon; no real picture needed.)
